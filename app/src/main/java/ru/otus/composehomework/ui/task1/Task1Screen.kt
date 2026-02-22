5678ec4c901a69201e389a937f25f408696f9446
Karavaev <karavaev@inpas.ru>
2026-02-23 01:07:53 +0300

    **Задание 1**: Переписывание XML на Compose (базовые компоненты)

--- app/src/main/java/ru/otus/composehomework/ui/task1/Task1Screen.kt
+++ app/src/main/java/ru/otus/composehomework/ui/task1/Task1Screen.kt
@@ -1,6 +1,16 @@
 package ru.otus.composehomework.ui.task1
 
 import androidx.compose.runtime.Composable
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.material3.*
+import androidx.compose.runtime.*
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 
 /**
  * Задание 1: Переписывание XML на Compose
@@ -22,5 +32,31 @@ import androidx.compose.runtime.Composable
  */
 @Composable
 fun Task1Screen() {
-    // TODO: Реализуйте экран на Compose
+    var welcomeText by remember { mutableStateOf("Добро пожаловать!") }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.spacedBy(32.dp)
+    ) {
+        Text(
+            text = welcomeText,
+            fontSize = 24.sp,
+            fontWeight = FontWeight.Bold
+        )
+
+        Image(
+            painter = painterResource(id = android.R.drawable.ic_dialog_info),
+            contentDescription = "Welcome",
+            modifier = Modifier.size(120.dp)
+        )
+
+        Button(
+            onClick = { welcomeText = "Кнопка нажата!" }
+        ) {
+            Text("Нажми меня")
+        }
+    }
 }
